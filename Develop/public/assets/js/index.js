54db5ad52c6cbb1df916a747a5b7b2d2f1b0e4f8
Radhika Shetti <Radhika.shetti@nortonlifelock.com>
2023-05-07 12:38:14 -0700

    adding take notes and delete notes methods

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -71,6 +71,8 @@ const handleNoteSave = () => {
     title: noteTitle.value,
     text: noteText.value,
   };
+
+  console.log('newNote',newNote)
   saveNote(newNote).then(() => {
     getAndRenderNotes();
     renderActiveNote();
@@ -119,6 +121,7 @@ const handleRenderSaveBtn = () => {
 // Render the list of note titles
 const renderNoteList = async (notes) => {
   let jsonNotes = await notes.json();
+  console.log('jsonNotes',jsonNotes)
   if (window.location.pathname === '/notes') {
     noteList.forEach((el) => (el.innerHTML = ''));
   }
--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,5 @@
 const express= require('express')
-
+const fs= require('fs')
 const path = require("path");
 const PORT = 3001;
 
@@ -28,6 +28,56 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+//takeNotes and write it to db.json file
+function takeNotes(body, allNotes) {
+  const newNote = body;
+
+  console.log('newNote',newNote)
+  if (!Array.isArray(allNotes))
+    allNotes = [];
+
+  if (allNotes.length === 0)
+    allNotes.push(0);
+
+  body.id = allNotes[0];
+  allNotes[0]++;
+
+  allNotes.push(newNote);
+  console.log('notesArray',allNotes)
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify(allNotes, null, 2)
+  );
+  return newNote;
+}
+
+app.post('/api/notes', (req, res) => {
+  const newNote = takeNotes(req.body, allNotes);
+  res.json(newNote);
+});
+
+
+//delete notes- update db.json
+function deleteNote(id, allNotes) {
+  for (let i = 0; i < allNotes.length; i++) {
+    let note = allNotes[i];
+
+    if (note.id == id) {
+      allNotes.splice(i, 1);
+      console.log('notesArray in delete',allNotes)
+      fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(allNotes, null, 2)
+      );
+      break;
+    }
+  }
+}
+
+app.delete('/api/notes/:id', (req, res) => {
+  deleteNote(req.params.id, allNotes);
+  res.json(true);
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
